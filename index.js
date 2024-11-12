5a229c242634442a4ca11a4a1e88f4b5be285200
m-shree <mjeevithashree@gmail.com>
2024-11-12 22:12:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function () {
+    const hamIcon = this.querySelector('.hamburger-icon');
+    const crossIcon = this.querySelector('.cross-icon');
+    if (hamIcon.style.display === "none") {
+        hamIcon.style.display = "inline-block"
+        menu.style.display = "none"
+        crossIcon.style.display = "none"
+    }
+    else {
+        crossIcon.style.display = "inline-block"
+        hamIcon.style.display = "none"
+        menu.style.display = "block"
+    }
+    const fadeInElements = document.querySelectorAll('.fade-in');
+window.addEventListener('scroll', () => {
+    fadeInElements.forEach(el => {
+        const rect = el.getBoundingClientRect();
+        if (rect.top < window.innerHeight - 100) {
+            el.classList.add('show');
+        }
+    });
+});
+
+});
